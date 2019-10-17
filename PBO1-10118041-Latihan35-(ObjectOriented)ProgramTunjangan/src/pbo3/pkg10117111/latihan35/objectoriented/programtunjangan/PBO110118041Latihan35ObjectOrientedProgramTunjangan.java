c531dd67141332c3cf9508001e63b85606252d85
ekisalman03 <55838966+ekisalman03@users.noreply.github.com>
2019-10-17 22:34:40 +0700

    Add files via upload

---
+++ PBO1-10118041-Latihan35-(ObjectOriented)ProgramTunjangan/src/pbo3/pkg10117111/latihan35/objectoriented/programtunjangan/PBO110118041Latihan35ObjectOrientedProgramTunjangan.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117111.latihan35.objectoriented.programtunjangan;
+
+/**
+ *
+ * @author Asus
+ */
+public class PBO110118041Latihan35ObjectOrientedProgramTunjangan {
+    
+}
